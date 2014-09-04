2859cd8a4368ab4cde64a7bc9486255d00606d8a
nikunjchapadia <nikunj.chapadia@gmail.com>
2014-09-03 23:21:10 -0700

    added multiple views in one view

--- app/src/main/java/com/wiwly/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/wiwly/sunshine/ForecastAdapter.java
@@ -15,13 +15,26 @@ import android.widget.TextView;
  */
 public class ForecastAdapter extends CursorAdapter {
 
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forcast, parent, false);
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        if(viewType == VIEW_TYPE_TODAY){
+            layoutId = R.layout.list_item_forecast_today;
+        }else {
+            layoutId = R.layout.list_item_forcast;
+        }
+        //return LayoutInflater.from(context).inflate(R.layout.list_item_forcast, parent, false);
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     @Override
@@ -57,4 +70,15 @@ public class ForecastAdapter extends CursorAdapter {
         TextView lowTextView = (TextView) view.findViewById(R.id.list_item_low_text_view);
         lowTextView.setText(Utility.formatTemperature(low, isMetric));
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
 }
